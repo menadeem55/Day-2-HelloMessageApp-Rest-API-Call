8cbc520920eaf49dab898dde11dccba04081ca86
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-04-07 01:09:07 +0530

    Added: UC-1 Creating a Rest Controller to demonstrate various Http Method

--- src/main/java/com/bridgelabz/spring/controller/Controller.java
+++ src/main/java/com/bridgelabz/spring/controller/Controller.java
@@ -3,5 +3,13 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Controller {
-
+    //UC-1
+    @RequestMapping(value = "/hello")
+    public String hello(){
+        return "Hello from Bridgelbaz";
+    }
+    @RequestMapping(value = "/nadeem")
+    public String message(){
+        return "Hello from Nadeem";
+    }
 }
